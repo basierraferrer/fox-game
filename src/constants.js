356839d72cd9e9e2db7cf01efee09b46090fc909
Brian Sierra <basierraferrer@gmail.com>
2024-08-07 11:54:51 -0500

    Added more constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,49 @@
+export const DAY_LENGTH = 60;
+
+export const ICONS = {
+  FISH: "fish",
+  POOP: "poop",
+  WEATHER: "weather",
+};
+
+export const ICONS_ARRAY = [ICONS.FISH, ICONS.POOP, ICONS.WEATHER];
+
+export const MOD_FOX = {
+  CELEBRATE: "celebrate",
+  DIE: "die",
+  EATING: "eating",
+  EGG: "egg",
+  HUNGRY: "hungry",
+  IDLING: "idling",
+  POOPING: "pooping",
+  RAIN: "rain",
+  SLEEP: "sleep",
+};
+
+export const MOD_SCENE = {
+  DAY: "day",
+  NIGHT: "night",
+  RAIN: "rain",
+  RIP: "dead",
+};
+
+export const SCENES = [MOD_SCENE.DAY, MOD_SCENE.RAIN];
+
+export const NIGHT_LENGTH = 3;
+
+export const RAIN_CHANCE = 0.5;
+
+export const STATES = {
+  CELEBRATING: "CELEBRATING",
+  DEAD: "DEAD",
+  EATING: "EATING",
+  FEEDING: "FEEDING",
+  HATCHING: "HATCHING",
+  HUNGRY: "HUNGRY",
+  IDLING: "IDLING",
+  INIT: "INIT",
+  POOPING: "POOPING",
+  SLEEPING: "SLEEPING",
+};
+
 export const TICK_RATE = 3000;
